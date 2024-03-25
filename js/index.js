f63c8a8b1e2f02d378bba6f41dea0f9a84065252
Ruwani Vidanachchi <t3vi00@students.oamk.fi>
2024-03-25 16:22:49 +0200

    change the back-end url

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const BACKEND_ROOT_URL = 'http://localhost:3001';
+const BACKEND_ROOT_URL = 'https://todo-backend-fzpk.onrender.com';
 import { Todos } from "./class/Todo.js";
 
 const todos = new Todos(BACKEND_ROOT_URL);
